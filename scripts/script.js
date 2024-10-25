57bad0cc45d42fd8ab9a9822628d92b59d680828
hananeltassa <hananeltassa15@gmail.com>
2024-10-25 21:55:42 +0300

    game

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,9 @@ window.onload = function () {
     const end = document.getElementById("end");
     const status = document.getElementById("status"); //this element shows the game status if you win or lose
     const boundaries = document.querySelectorAll(".boundary"); // the walls in the maze (.boundary cz we use it for the class)
+    const gameArea = document.getElementById("game")
+
+    let playing = false;
 
 
     console.log(start);
@@ -16,19 +19,37 @@ window.onload = function () {
     console.log(boundaries);
 
     function startgame(){
+        playing = true; // to set the game has started
         status.textContent = "Game in progress..." // updates the status text
         console.log("game start");
     }
 
     function loseGame(){
-        status.textContent = "You Lost! Try Again :( "
-        console.log("Lost the game");
+        if (playing) { // if true 
+            playing = false;
+            status.textContent = "You Lost! Try Again :( "
+            console.log("Lost the game");
+    
+            boundaries.forEach(boundary => {
+                boundary.classList.add("highlighted")  // this changes the class for boundaries to change the bk-color
+            });
 
-        boundaries.forEach(boundary => {
-            boundary.classList.add("highlighted")  // this changes the class for boundaries to change the bk-color
-            
-        });
+            setTimeout(resetGame, 2000);  // to reset the game
+        }
     }
+
+
+    function resetGame(){
+        status.textContent = "Move your mouse over the 'S' ";
+        console.log("Again")
+
+        //we need to remove the highlights again
+        boundaries.forEach(boundary => {
+            boundary.classList.remove("highlighted")  // this changes the class for boundaries to change the bk-color
+        });
+    
+    }
+
 
     function winGame(){
         status.textContent = "Congratulation You Won !!"
@@ -43,7 +64,7 @@ window.onload = function () {
     
     end.addEventListener("mouseover", winGame);
 
-    
+    gameArea.addEventListener("mouseleave", loseGame);
 
 
 }
