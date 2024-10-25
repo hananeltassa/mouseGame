b3a4dbf90d68316682fd75c7ff54bd686dd7198d
hananeltassa <hananeltassa15@gmail.com>
2024-10-25 21:32:34 +0300

    startgame, losegame

--- scripts/script.js
+++ scripts/script.js
@@ -7,9 +7,43 @@ window.onload = function () {
     const start = document.getElementById("start");
     const end = document.getElementById("end");
     const status = document.getElementById("status"); //this element shows the game status if you win or lose
-    const boundary = document.getElementById("boundary"); // the walls in the maze
-
+    const boundaries = document.querySelectorAll(".boundary"); // the walls in the maze (.boundary cz we use it for the class)
 
 
     console.log(start);
+    console.log(end);
+    console.log(status);
+    console.log(boundaries);
+
+    function startgame(){
+        status.textContent = "Game in progress..." // updates the status text
+        console.log("game start");
+    }
+
+    function loseGame(){
+        status.textContent = "You Lost! Try Again :( "
+        console.log("Lost the game");
+
+        boundaries.forEach(boundary => {
+            boundary.classList.add("highlighted")  // this changes the class for boundaries to change the bk-color
+            
+        });
+    }
+
+    function winGame(){
+        status.textContent = "Congratulation You Won !!"
+        console.log("end game");
+    }
+
+    start.addEventListener("mouseover", startgame);
+
+    boundaries.forEach(boundary =>{
+        boundary.addEventListener("mouseover", loseGame )
+    })
+    
+    end.addEventListener("mouseover", winGame);
+
+    
+
+
 }
